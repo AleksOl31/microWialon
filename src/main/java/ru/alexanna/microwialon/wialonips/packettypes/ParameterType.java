2a13554115fffaa3a18302334d2396c5584a6393
Oleg <procundo@yandex.ru>
2022-01-18 21:45:41 +0300

    First commit in home 18.01.2022

--- src/main/java/ru/alexanna/microwialon/wialonips/packettypes/ParameterType.java
+++ src/main/java/ru/alexanna/microwialon/wialonips/packettypes/ParameterType.java
@@ -3,7 +3,7 @@ package ru.alexanna.microwialon.wialonips.packettypes;
 public enum ParameterType {
     Integer("1"), Double("2"), String("3");
 
-    private String value;
+    private final String value;
 
     ParameterType(String value) {
         this.value = value;
